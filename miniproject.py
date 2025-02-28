05b1189dfe23d3995f1b4c4c008d17c3878a6b4a
SamuelPalenik101 <samuel@blinktroll.com>
2025-02-28 10:13:33 +0100

    Got mask for pumpkins

--- miniproject.py
+++ miniproject.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import rasterio
 import scipy
+from enum import Enum
 
 class COLOR_SPACE(Enum):
 	CIELAB = 1
-	HSL = 2
+	HLS = 2
 	HSV = 3
 	RGB = 4
 
@@ -15,6 +16,10 @@ class miniproject:
 	def __init__(self, path_image = None, path_annotated=None):
 		self.annotated_image = None
 		self.image = None
+		self.mask = None
+  
+		self.mean = 0
+		self.std = 0
 		
 		if path_image:
 			self.image = self.open_image(path_image)
@@ -25,33 +30,37 @@ class miniproject:
 		image = cv.imread(path)
 		return image
 
-	def change_color_space(self, image, color_space):
+	def change_color_space(self, color_space):
 		
 		if color_space == COLOR_SPACE.CIELAB:
-			image = cv.cvtColor(image, cv.COLOR_BGR2LAB)
-		elif color_space == COLOR_SPACE.HSL:
-			image = cv.cvtColor(image, cv.COLOR_BGR2HLS)
+			self.image = cv.cvtColor(self.image, cv.COLOR_BGR2LAB)
+		elif color_space == COLOR_SPACE.HLS:
+			self.image = cv.cvtColor(self.image, cv.COLOR_BGR2HLS)
 		elif color_space == COLOR_SPACE.HSV:
-			image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+			self.image = cv.cvtColor(self.image, cv.COLOR_BGR2HSV)
 		elif color_space == COLOR_SPACE.RGB:
-			image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+			self.image = cv.cvtColor(self.image, cv.COLOR_BGR2RGB)
 		else:
 			print("Imcorrect color space")
-			return None
 
-		return image
 
-	def create_mask(self, lower_band, upper_band):
-		mask = cv.inRange(self.annotated_image, lower_band, upper_band)
+	def create_mask(self, lower_bound, upper_bound):
+		self.mask = cv.inRange(self.annotated_image, lower_bound, upper_bound)
 
-		mean, std = cv.meanStdDev(self.image, mask = mask)
-		print(mean)
-	
+		self.mean, self.std = cv.meanStdDev(self.image, self.mask)
 
 
 if __name__ == "__main__":
-	project = miniproject()
-	image = project.open_image('figures/pumpkin_annottated.JPG')
-	cv.imshow("annotated", image)
+	project = miniproject('figures/EB-02-660_0595_0068.JPG','figures/pumpkin_annottated.JPG')
+
+	project.change_color_space(COLOR_SPACE.HLS)
+
+	red_lower = (0, 0, 200)
+	red_upper = (100, 100, 255)
+	project.create_mask(red_lower, red_upper)
+ 
+	cv.imwrite("figures/output/mask.jpg", project.mask)
+ 
+	cv.imshow("annotated", project.mask)
 	cv.waitKey(0)
 	cv.destroyAllWindows()

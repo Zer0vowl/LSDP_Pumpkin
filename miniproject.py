a1a6d8024a8f4136d2cb0397707ed2fe1366ca39
SamuelPalenik101 <samuel@blinktroll.com>
2025-02-28 09:16:30 +0100

    Initial commit

---
+++ miniproject.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import rasterio
+import scipy
+
+
+class miniproject:
+    
+    def __init__(self):
+        pass
+    
+    def open_image(self):
+        pass

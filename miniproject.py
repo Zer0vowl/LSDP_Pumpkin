4b6cfe3d4506d043366551662cf5ea8040a266d7
SamuelPalenik101 <samuel@blinktroll.com>
2025-02-28 09:32:40 +0100

    changes to the code

--- miniproject.py
+++ miniproject.py
@@ -4,22 +4,29 @@ import matplotlib.pyplot as plt
 import rasterio
 import scipy
 
+class COLOR_SPACE(Enum):
+    CIELAB = 1
+    HSL = 2
+    HSV = 3
+    RGB = 4
 
 class miniproject:
     
     def __init__(self):
         self.annotated_image = None
         
-    
+
     def open_image(self, path):
         image = cv.imread(path)
         return image
-    
-    
+
+    def segment(self):
+        pass
+
+
 if __name__ == "__main__":
     project = miniproject()
     image = project.open_image('figures/pumpkin_annottated.JPG')
     cv.imshow("annotated", image)
     cv.waitKey(0)
     cv.destroyAllWindows()
-    

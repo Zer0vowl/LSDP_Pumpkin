d2f1a7258c8609fe9cd094d73e95a89f5c97f690
Filip Lobpreis <f.lobpreis@gmail.com>
2025-02-28 18:52:45 +0100

    cp: miniproject: refactor code
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- miniproject.py
+++ miniproject.py
@@ -5,7 +5,7 @@ import rasterio
 import scipy
 from enum import Enum
 
-class COLOR_SPACE(Enum):
+class ColorSpace(Enum):
 	CIELAB = 1
 	HLS = 2
 	HSV = 3
@@ -20,6 +20,7 @@ class miniproject:
 		self.mean = 0
 		self.ref_image = None
 		self.std = 0
+		self.colorspace = ColorSpace.RGB
 
 		if tif_image:
 			self.image = self.open_image(tif_image)
@@ -38,19 +39,19 @@ class miniproject:
 
 
 	def change_color_space(self, color_space):
-		if color_space == COLOR_SPACE.CIELAB:
+		if color_space == ColorSpace.CIELAB:
 			self.ref_image = cv.cvtColor(self.ref_image, cv.COLOR_BGR2LAB)
-		elif color_space == COLOR_SPACE.HLS:
+		elif color_space == ColorSpace.HLS:
 			self.ref_image = cv.cvtColor(self.ref_image, cv.COLOR_BGR2HLS)
-		elif color_space == COLOR_SPACE.HSV:
+		elif color_space == ColorSpace.HSV:
 			self.ref_image = cv.cvtColor(self.ref_image, cv.COLOR_BGR2HSV)
-		elif color_space == COLOR_SPACE.RGB:
+		elif color_space == ColorSpace.RGB:
 			self.ref_image = cv.cvtColor(self.ref_image, cv.COLOR_BGR2RGB)
 		else:
 			print("Imcorrect color space")
 
 
-	def create_mask(self, image, lower_bound, upper_bound):
+	def threshold(self, image, lower_bound, upper_bound):
 		self.mask = cv.inRange(image, lower_bound, upper_bound)
 
 		self.mean, self.std = cv.meanStdDev(image, self.mask)
@@ -58,26 +59,34 @@ class miniproject:
 		""" print(self.mean) """
 
 
-	def mahalanobis_distance(self, annotated_mask):
+	def mahalanobis_distance_mask(self):
+		# Threshold the annotated image to get the mask
+		red_lower = (0, 0, 200)
+		red_upper = (100, 100, 255)
+		annotated_mask = self.threshold(self.ref_image_annotated, red_lower, red_upper)
+
+		# Extract the pixels from the annotated mask
 		indices = np.where(annotated_mask > 0)
 		extracted_pixels = project.ref_image[indices[0], indices[1]]
+
+		# Calculate the mean and covariance matrix of the extracted pixels
 		mean = np.mean(extracted_pixels, axis=0)
 		covar, _ = cv.calcCovarMatrix(extracted_pixels, mean, cv.COVAR_NORMAL | cv.COVAR_ROWS | cv.COVAR_SCALE)
 
+		# Calculate the Mahalanobis distance
 		pixels = np.reshape(self.ref_image, (-1, 3))
 		diff = pixels - np.repeat([mean], pixels.shape[0], axis=0)
 		inv_cov = np.linalg.inv(covar)
 		moddotproduct = diff * (diff @ inv_cov)
 		mahalanobis_dist = np.sum(moddotproduct, axis=1)
 		mahalanobis_distance_image = np.reshape(mahalanobis_dist, (self.ref_image.shape[0], self.ref_image.shape[1]))
+
+		# The image contains values between 0 and 255. Extract only the values below 10. Set them to 255. The rest to 0.
 		cv.imwrite("figures/output/mahalanobis_distance_image.jpg", mahalanobis_distance_image)
 		cv.threshold(mahalanobis_distance_image, 10, 255, cv.THRESH_BINARY_INV, mahalanobis_distance_image)
 
 		cv.imwrite("figures/output/mahalanobis_distance_image_thrashold.jpg", mahalanobis_distance_image)
-		return mahalanobis_distance_image
-
-
-
+		return mahalanobis_distance_image.astype(np.uint8)
 
 
 if __name__ == "__main__":
@@ -86,20 +95,14 @@ if __name__ == "__main__":
 		Warning("Image not loaded")
 		exit(1)
 
-	""" project.change_color_space(COLOR_SPACE.HLS) """
+	project.change_color_space(ColorSpace.HLS)
 
-	red_lower = (0, 0, 200)
-	red_upper = (100, 100, 255)
-	annotated_mask = project.create_mask(project.ref_image_annotated, red_lower, red_upper)
-	mahalanobis_mask = project.mahalanobis_distance(annotated_mask).astype(np.uint8)
-	print(mahalanobis_mask.shape, mahalanobis_mask.dtype)
+	mahalanobis_mask = project.mahalanobis_distance_mask()
 	masked_image = cv.bitwise_and(project.ref_image, project.ref_image, mask=mahalanobis_mask)
 
 	# Only take BGR values
 	mean_color = cv.mean(project.ref_image, mask=mahalanobis_mask)
-
 	mean, stddev = cv.meanStdDev(project.ref_image, mask=mahalanobis_mask)
-
 
 	# Convert from 2D array to tuple
 	mean = tuple(mean.flatten())
@@ -111,15 +114,14 @@ if __name__ == "__main__":
 	gain = 4
 	low_orange = np.array([a - b for a, b in zip(mean, stddev)])
 	upper_orange = np.array([a + b for a, b in zip(mean, stddev)])
-	""" print(low_orange, upper_orange) """
 
-	global_mask = project.create_mask(project.image, low_orange, upper_orange)
+	global_mask = project.threshold(project.image, low_orange, upper_orange)
 	cv.imwrite("figures/output/global_mask.jpg", global_mask)
 
-	closed_image = cv.dilate(global_mask, None, iterations=1)
-	cv.imwrite("figures/output/global_mask_dilated.jpg", closed_image)
+	global_mask = cv.dilate(global_mask, None, iterations=1)
+	cv.imwrite("figures/output/global_mask_dilated.jpg", global_mask)
 
-	out = cv.bitwise_and(project.image, project.image, mask=closed_image)
+	out = cv.bitwise_and(project.image, project.image, mask=global_mask)
 	cv.imwrite("figures/output/out.jpg", out)
 
 	cv.imwrite("figures/output/mask.jpg", project.mask)
@@ -129,7 +131,7 @@ if __name__ == "__main__":
 	""" cv.waitKey(0) """
 	cv.destroyAllWindows()
 
-	contours, hierarchy = cv.findContours(closed_image, cv.RETR_TREE,
+	contours, hierarchy = cv.findContours(global_mask, cv.RETR_TREE,
 			cv.CHAIN_APPROX_SIMPLE)
 
 	# Draw a circle above the center of each of the detected contours.

261fe6833acee371d70a34400cedbdcafd08928e
Filip Lobpreis <f.lobpreis@gmail.com>
2025-02-28 11:28:49 +0100

    miniproject: create global mask
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- miniproject.py
+++ miniproject.py
@@ -12,17 +12,18 @@ class COLOR_SPACE(Enum):
 	RGB = 4
 
 class miniproject:
-	
+
 	def __init__(self, path_image = None, path_annotated=None):
 		self.annotated_image = None
 		self.image = None
 		self.mask = None
-  
+
 		self.mean = 0
 		self.std = 0
-		
+
 		if path_image:
 			self.image = self.open_image(path_image)
+			self.image = cv.GaussianBlur(self.image, (5, 5), 0)
 		if path_annotated:
 			self.annotated_image = self.open_image(path_annotated)
 
@@ -47,25 +48,29 @@ class miniproject:
 		self.mask = cv.inRange(image, lower_bound, upper_bound)
 
 		self.mean, self.std = cv.meanStdDev(image, self.mask)
-		print(self.mean)
+		return self.mask
+		""" print(self.mean) """
 
 
 if __name__ == "__main__":
 	project = miniproject('figures/EB-02-660_0595_0068.JPG','figures/pumpkin_annottated.JPG')
+	if project.image is None:
+		Warning("Image not loaded")
+		exit(1)
 
-	project.change_color_space(COLOR_SPACE.HLS)
+	""" project.change_color_space(COLOR_SPACE.HLS) """
 
 	red_lower = (0, 0, 200)
 	red_upper = (100, 100, 255)
-	project.create_mask(project.annotated_image, red_lower, red_upper)
+	annotated_mask = project.create_mask(project.annotated_image, red_lower, red_upper)
 
 
 	masked_image = None
-	masked_image = cv.bitwise_and(project.image, project.image, mask=project.mask)
-	
-	print(type(masked_image))
+	masked_image = cv.bitwise_and(project.image, project.image, mask=annotated_mask)
+
+	""" print(type(masked_image)) """
 	mean_color = cv.mean(project.image, mask=project.mask)  # Only take BGR values
- 
+
 	mean, stddev = cv.meanStdDev(project.image, mask=project.mask)
 
 	# Convert from 2D array to tuple
@@ -73,14 +78,29 @@ if __name__ == "__main__":
 	stddev = tuple(stddev.flatten())
 	stddev_half = [a/2 for a in stddev]
 
-	#low_orange = tuple(a - b for a, b in zip(mean, std)
+	# Setup the lower and upper bounds for the mask.
+	# The color is dependent on the mean and standard deviation of the values created by the annotated image.
+	gain = 4
+	low_orange = np.array([a - b for a, b in zip(mean, stddev*gain)])
+	upper_orange = np.array([a + b for a, b in zip(mean, stddev*gain)])
+	print(low_orange, upper_orange)
 
-	print(stddev)
-	print(stddev_half)
+	global_mask = project.create_mask(project.image, low_orange, upper_orange)
+	cv.imwrite("figures/output/global_mask.jpg", global_mask)
+
+	closed_image = cv.dilate(global_mask, None, iterations=4)
+	cv.imwrite("figures/output/global_mask_dilated.jpg", closed_image)
+
+	""" kernel = np.ones((10, 10), np.uint8) """
+	""" closed_image = cv.morphologyEx(global_mask, cv.MORPH_CLOSE, kernel) """
+	""" cv.imwrite("figures/output/closed_image.jpg", closed_image) """
+
+	out = cv.bitwise_and(project.image, project.image, mask=closed_image)
+	cv.imwrite("figures/output/out.jpg", out)
 
 	cv.imwrite("figures/output/mask.jpg", project.mask)
 	cv.imwrite("figures/output/masked_image.jpg", masked_image)
 
-	cv.imshow("annotated", masked_image)
-	cv.waitKey(0)
+	""" cv.imshow("annotated", masked_image) """
+	""" cv.waitKey(0) """
 	cv.destroyAllWindows()

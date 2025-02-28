d821b3f43b65dd1be2e9b66f89889abde7d41361
SamuelPalenik101 <samuel@blinktroll.com>
2025-02-28 09:43:06 +0100

    Added function for changing color spaces

--- miniproject.py
+++ miniproject.py
@@ -12,16 +12,35 @@ class COLOR_SPACE(Enum):
 
 class miniproject:
     
-    def __init__(self):
+    def __init__(self, path_image = None, path_annotated=None):
         self.annotated_image = None
+        self.image = None
         
+        if path_image:
+            self.image = self.open_image(path_image)
+        if path_annotated:
+            self.annotated_image = self.open_image(path_annotated)
 
     def open_image(self, path):
         image = cv.imread(path)
         return image
 
-    def segment(self):
-        pass
+    def change_color_space(self, image, color_space):
+        
+        if color_space == COLOR_SPACE.CIELAB:
+            image = cv.cvtColor(image, cv.COLOR_BGR2LAB)
+        elif color_space == COLOR_SPACE.HSL:
+            image = cv.cvtColor(image, cv.COLOR_BGR2HLS)
+        elif color_space == COLOR_SPACE.HSV:
+            image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+        elif color_space == COLOR_SPACE.RGB:
+            image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+        else:
+            print("Imcorrect color space")
+            return None
+
+        return image
+
 
 
 if __name__ == "__main__":
